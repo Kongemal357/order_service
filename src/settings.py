34d635487ea0b4124372f4c2d4b00d47380a93ca
yuriy <kongemal357@gmail.com>
2026-08-26 18:20:46 +0300

    fix settings

--- src/settings.py
+++ src/settings.py
@@ -11,7 +11,11 @@ class Settings(BaseSettings):
     PORT: int = 8000
 
     # Database
-    DATABASE_URL: str
+    POSTGRES_DATABASE_NAME: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_USERNAME: str
+    POSTGRES_PASSWORD: str
 
     # Capashino Services
     CAPASHINO_BASE_URL: str
@@ -20,6 +24,10 @@ class Settings(BaseSettings):
 
     # Kafka
     KAFKA_BOOTSTRAP_SERVERS: str = "kafka.kafka.svc.cluster.local:9092"
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.POSTGRES_USERNAME}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DATABASE_NAME}"
 
     class Config:
         env_file = ".env"
