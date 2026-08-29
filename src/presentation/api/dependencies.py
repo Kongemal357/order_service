7dc6de2ee46d6d267ea9536e00809e9769a139e5
yuriy <kongemal357@gmail.com>
2026-08-29 11:56:26 +0300

    add payment dependencies

--- src/presentation/api/dependencies.py
+++ src/presentation/api/dependencies.py
@@ -2,8 +2,11 @@ from typing import AsyncGenerator
 
 from fastapi import Depends
 from src.application.ports import CatalogClient, UnitOfWork
+from src.application.ports.payment_client import PaymentClient
 from src.application.usecases import CreateOrderUseCase, GetOrderUseCase
+from src.application.usecases.process_payment_callback import ProcessPaymentCallbackUseCase
 from src.infrastructure.http.catalog_client import CatalogHTTPClient
+from src.infrastructure.http.payment_client import PaymentHTTPClient
 from src.infrastructure.persistence.uow import SQLAlchemyUnitOfWork
 
 
@@ -18,12 +21,18 @@ def get_catalog_client() -> CatalogClient:
     return CatalogHTTPClient()
 
 
+def get_payment_client() -> PaymentClient:
+    """Dependency for Payment Service client."""
+    return PaymentHTTPClient()
+
+
 async def get_create_order_use_case(
     uow: UnitOfWork = Depends(get_uow),
     catalog_client: CatalogClient = Depends(get_catalog_client),
+    payment_client: PaymentClient = Depends(get_payment_client),
 ) -> CreateOrderUseCase:
     """Dependency for create order use case."""
-    return CreateOrderUseCase(uow, catalog_client)
+    return CreateOrderUseCase(uow, catalog_client, payment_client)
 
 
 async def get_get_order_use_case(
@@ -31,3 +40,10 @@ async def get_get_order_use_case(
 ) -> GetOrderUseCase:
     """Dependency for get order use case."""
     return GetOrderUseCase(uow)
+
+
+async def get_process_payment_callback_use_case(
+    uow: UnitOfWork = Depends(get_uow),
+) -> ProcessPaymentCallbackUseCase:
+    """Dependency for process payment callback use case."""
+    return ProcessPaymentCallbackUseCase(uow)
