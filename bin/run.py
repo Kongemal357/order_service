aa6ef7b3806829792c02e975905d1d32d442efa7
yuriy <kongemal357@gmail.com>
2026-09-10 13:24:35 +0300

    added upgrade to correct migration

--- bin/run.py
+++ bin/run.py
@@ -33,7 +33,7 @@ def run_migrations():
 
         alembic_cfg = Config("alembic.ini")
         command.downgrade(alembic_cfg, "ff6bdd83cbeb")
-        command.upgrade(alembic_cfg, "head")
+        command.upgrade(alembic_cfg, "e8170c01cdbc")
         logger.info("Migrations applied successfully")
     except ImportError:
         logger.warning(" Alembic not installed, skipping migrations")
